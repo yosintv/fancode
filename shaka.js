d0e618a51c0a0a8fded1775178312ed148495ed7
YoSin TV <61265398+yosintv@users.noreply.github.com>
2026-02-13 00:31:10 +0900

    Update shaka.js

--- shaka.js
+++ shaka.js
@@ -1,21 +1,28 @@
 const playlist = {
+
+
+    //HLS Links Only
+    "test": { url: "________"},
+    
+    "hls1": { url: " https://pull.niues.live/live/stream-400722_lhd.m3u8?auth_key=1770908855-0-0-e4f9e66cd41579f62ade48713084e796 "},
+    "hls2": { url: " https://pull.niur.live/live/stream-570313_lhd.m3u8?txSecret=c97e1b5c5946db86c7a6f7c91b58aef8&txTime=698e42f7 "}
+        
+    
+    
     "primehindi": {
         url: "https://a166aivottlinear-a.akamaihd.net/OTTB/bom-nitro/live/dash/enc/igjuwt107o/out/v1/489e489428ce4992b9bd784d6b948ab3/cenc.mpd",
         clearKeys: { "b07e9785466b831915621dc51ed50d3c": "af30a9d18114259f21a3cc9e34ff649d" }
     },
+
     "primecricket": {
         url: "https://a47live-pv-ta-amazon.akamaized.net/syd-nitro/live/clients/dash-sd/enc/q5sh0joslk/out/v1/a3efd10a95644817a2b8dff0c33b1b61/cenc-sd.mpd",
         clearKeys: { "aaf668d9b6766cd98665bfe90f843751": "aaf668d9b6766cd98665bfe90f843751" }
     },
+
     "laligatv": {
         url: "https://a166aivottlinear-a.akamaihd.net/OTTB/dub-nitro/live/clients/dash/enc/k0duzgfejg/out/v1/70a50b1bda944628b8e7e66ab4069419/cenc.mpd",
         clearKeys: { "620e51b82596475517a27aa425c52280": "2b9ba811e9c5aeafc8ae1b71cdca4d6a" }
-    },
-
-    //HLS Links Only
-    
-    "hls1": { url: "https://pull.niues.live/live/stream-400722_lhd.m3u8?auth_key=1770908855-0-0-e4f9e66cd41579f62ade48713084e796"},
-    "hls2": { url: "https://pull.niur.live/live/stream-491873_lhd.m3u8?txSecret=502577ca7bafb34fc1c7d832cccd03b2&txTime=698e427f"}
+    }
 };
 
 async function initPlayer() {
