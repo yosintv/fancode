1631f9fcb1deeb2325a56061eb8612677c8d8eee
YoSin TV <61265398+yosintv@users.noreply.github.com>
2026-06-14 08:57:08 +0900

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -16,7 +16,9 @@
   const ALLOWED_HOSTS = [
     "www.cricfoot.net",
     "cricfoot.net",
+    "s2.hls-player.net",
     "s3.hls-player.net",
+    "s4.hls-player.net",
     "redirects.pages.dev",
     "cr7world.pages.dev",
   ];
