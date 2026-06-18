eddc2766ab4fc4401557e619cd1f0f5488a0991d
YoSin TV <61265398+yosintv@users.noreply.github.com>
2026-06-19 03:14:55 +0900

    Update mpd.js

--- mpd/mpd.js
+++ mpd/mpd.js
@@ -2,7 +2,8 @@ foxalt,
 https://a35live-pv-ta-amazon.akamaized.net/iad-nitro/live/clients/dash/enc/pfcxjf1zas/out/v1/31d30c91fc65458789b84209d3fa22e4/cenc.mpd, 
 1f68713028d439ec03be07f56c1d6213:20093db6455160fffed4c394def3193d
 
-foxalt2, https://another-stream-url.com/live/index.mpd, abc123keyid67890:def456keyvalue0987
 
 elta,
-https://live.666666.zip/stream/86597104_okehlpipbdoz2lujaxkrvv9eylmlbduv.m3u8
+https://live.666666.zip/stream/86597104_okehlpipbdoz2lujaxkrvv9eylmlbduv.m3u8,
+
+
