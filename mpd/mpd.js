bd06cf0d0a95dc9a1fdfc99657eebcc4a7a0dcaa
YoSin TV <61265398+yosintv@users.noreply.github.com>
2026-06-19 03:32:49 +0900

    Update mpd.js

--- mpd/mpd.js
+++ mpd/mpd.js
@@ -2,10 +2,6 @@ foxalt,
 https://a35live-pv-ta-amazon.akamaized.net/iad-nitro/live/clients/dash/enc/pfcxjf1zas/out/v1/31d30c91fc65458789b84209d3fa22e4/cenc.mpd, 
 1f68713028d439ec03be07f56c1d6213:20093db6455160fffed4c394def3193d
 
-tsn3,
-https://live-pv-ta.amazon.fastly-edge.com/syd-nitro/live/clients/dash/enc/lzhmuzmjsl/out/v1/c171eeb214c749f2b351c79df317b21e/cenc.mpd,
-key1=f5c2b30eac11be1e8cdfc9585f5fe6af:a8198d17bf9b0da77450fbb919a38b2d
-
 tvp,
 https://estreams.tv.nej.cz/dash/CH_TVP_SPORT_Portable.ism/playlist.mpd,
 :
@@ -14,25 +10,7 @@ zdf,
 https://simplitv-live.mdn.ors.at/live/eds/zdf_hd/dash4h/zdf_hd.mpd,
 c1a0ac1044a433d0856ccdc08f245084:7f0e8800a6d63d7915ac181bb88ce813
 
+
 m6france,
 https://origin-m6web.live.6cloud.fr/out/v1/6play/6play-m6/cmaf_cenc00/dash-short-hd.mpd,
 433ffba670963e70857859a9dff4be04:51ede3a821229fe81e71282c8eff80e3
-
-telemundo,
-https://live-oneapp-prd-news.akamaized.net/Content/CMAF_OL2-CTR-4s-v2/Live/channel(kvea)/master.mpd,
-ce7ab3022e753307997f58afe001bac4:72d631a66e635c60829a0fe7705516c1
-
-sportv,
-https://a151aivottlinear-a.akamaihd.net/OTTB/sin-nitro/live/dash/enc/m7duvnk2bu/out/v1/d1ad69118b5647309b1eb7213affdb3d/cenc.mpd,
-4bbcff3289d457b4dd5dbdd21221de9a:c4906b9a9f8dda3c0725bddb8c497733
-
-fubo,
-https://hlsplayers.pages.dev/player3?url=https://live-manifest.production-public.tubi.io/live/d8c035df-1076-4aa6-8628-da2ec80781f9/playlist.m3u8
-
-sports1sk,
-https://dash2.antik.sk/stream/nvidia_sport_1/playlist_cenc.mpd,
-11223344556677889900112233445566:4b80724d0ef86bcb2c21f7999d67739d
-
-isra1,
-https://d1zqtf09wb8nt5.cloudfront.net/livedash/oil/freetv/live/sport_5/live.livx?indexMode&amp;dvr=7200000,
-c3b32b23081b5486ae5176da9c358808:4708b352ee1d50ccdf94af9d2a952ed8
