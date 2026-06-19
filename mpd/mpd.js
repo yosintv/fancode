a4e2aa5bd799e6cf86ceb71a5e177d4f05376a44
YoSin TV <61265398+yosintv@users.noreply.github.com>
2026-06-19 16:03:54 +0900

    Update mpd.js

--- mpd/mpd.js
+++ mpd/mpd.js
@@ -6,7 +6,6 @@ tvp,
 https://estreams.tv.nej.cz/dash/CH_TVP_SPORT_Portable.ism/playlist.mpd,
 :
 
-
 m6france,
 https://origin-m6web.live.6cloud.fr/out/v1/6play/6play-m6/cmaf_cenc00/dash-short-hd.mpd,
 433ffba670963e70857859a9dff4be04:51ede3a821229fe81e71282c8eff80e3
