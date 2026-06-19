a57821b17682edd2cd5fc2346f9bc4208f491562
YoSin TV <61265398+yosintv@users.noreply.github.com>
2026-06-20 04:07:14 +0900

    Update mpd.js

--- mpd/mpd.js
+++ mpd/mpd.js
@@ -14,4 +14,3 @@ https://origin-m6web.live.6cloud.fr/out/v1/6play/6play-m6/cmaf_cenc00/dash-short
 tsn4,
 https://otte-qw.live.pv-cdn.net/bom-nitro/live/clients/dash/enc/w0rehjjrwe/out/v1/69a2a7041395406b970598f61680e7cf/cenc.mpd,
 14eeabf30c14b7fbf3008c03099ce011:17d2ac8dbc5429bd70af3433aa12158d
-
