8c498925fbe764c2d2b87bfbe878d0ecf01b5402
YoSin TV <61265398+yosintv@users.noreply.github.com>
2026-07-01 01:51:15 +0900

    Update mpd.js

--- mpd/mpd.js
+++ mpd/mpd.js
@@ -8,16 +8,10 @@ https://qp-pldt-live-bpk-ucd-prod.akamaized.net/bpk-tv/fifa_ppv1/default/index.m
 2c338a117d434ce4bbe3569231af90f1:a9633d901ee8a3f4f58ac314b5c5f4fb
   
 
-
-ch1,
-https://otte-qw.live.pv-cdn.net/gru-nitro/live/clients/dash/enc/3gg2jnixjn/out/v1/e1840e01f3f14563b66bbb944d5cc54c/cenc.mpd,
-f8b207c10f3f76aeba32a360ec52b9e4:afad49d20eb39670e93e371c1d669921
-  
-
-ch2,
-https://otte-qw.live.pv-cdn.net/bom-nitro/live/clients/dash/enc/w0rehjjrwe/out/v1/69a2a7041395406b970598f61680e7cf/cenc.mpd,
+tsn1,
+https://otte.cache.aiv-cdn.net/bom-nitro/live/clients/dash/enc/w0rehjjrwe/out/v1/69a2a7041395406b970598f61680e7cf/cenc.mpd,
 14eeabf30c14b7fbf3008c03099ce011:17d2ac8dbc5429bd70af3433aa12158d
-
+  
 
 fox4k,
 https://otte-qw.live.pv-cdn.net/pdx-nitro/live/clients/dash/enc/ajfoeddkbz/out/v1/b78800b9b2304879b15843f455836829/cenc.mpd,
@@ -25,33 +19,14 @@ f6564ec2aee819046328a0e153be574d:ff46a8a1031eb27ef22576a077c98ab7
 
 
 
-foxalt, 
-https://otte-qw.live.pv-cdn.net/iad-nitro/live/dash/enc/ap5wz1ofsp/out/v1/7fa6feef143747beaa186ebb6dfb2532/cenc.mpd,
-c620c93c60c04999eb9ddc28ecfb70a8:e76a709c251313190e76cb3c3d3a5824
-
-vix,
-https://vod-cua-orbit2-s2.izzigo.tv/out/u/startover/dash/VIX-MUNDIAL-90-HD/default.mpd,
-75074a0cbd83f5b72fb3e5c1495252a9:a4a0df04b2d28f2160cead2ecdeb7203
-  
-tvp,
-https://estreams.tv.nej.cz/dash/CH_TVP_SPORT_Portable.ism/playlist.mpd,
-:
 
 
-
-
-tsn4,
-https://otte-qw.live.pv-cdn.net/bom-nitro/live/clients/dash/enc/w0rehjjrwe/out/v1/69a2a7041395406b970598f61680e7cf/cenc.mpd,
-14eeabf30c14b7fbf3008c03099ce011:17d2ac8dbc5429bd70af3433aa12158d
+/////////////////////////////////////////////////////////////////////////////// Workings till now//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 ctsports,
 https://dash2.antik.sk/stream/nvidia_ct_sport/playlist_cenc.mpd,
 11223344556677889900112233445566:4b80724d0ef86bcb2c21f7999d67739d
-
-sporza,
-https://live.vrtcdn.be/groupd/live/0761024f-37fe-4254-bc37-e95d7c62b2d1/playout.isml/.mpd,
-7c0a14fc7dc3c641af8cb6506431a628:f318ff7c01cca20c4a5b3d91074ca460
 
 
 
